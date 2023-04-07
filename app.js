093e9fbf452e622f43db42974e9fc9adc9c1d2cf
ufarwa <110379925+ufarwa@users.noreply.github.com>
2023-04-07 01:19:07 -0700

    JavaScript Assignment 1

---
+++ app.js
@@ -0,0 +1,112 @@
+//assignment of class 2
+//Q1) write a java script program that accepts two numbers in two prompts and display the larger one in the console.
+// let num1 = +prompt("enter 1st number");
+// let num2 = +prompt("enter 2nd number");
+// if(num1>num2){
+//     console.log("The larger number is " + num1);
+// }
+// else if(num1<num2){
+//     console.log("The larger number is " + num2);
+
+// }
+// else{
+//     console.log("Both are equal");
+// }
+
+//Q2) Write a JavaScript conditional statement to find the sign of number. Display an alert box with the sepified sign.
+// let number = +prompt("Enter a number");
+// if(number>0){
+//     console.log("the sign is +");
+// }
+// else if(number<0){
+//     console.log("the sign is -");
+// }
+// else{
+//     console.log("You are not entering a number.Please enter a  valid number");
+// }
+
+//Q3) Write a Javascript prigram that accepts five numbers in five prompts and display larger one in the console.
+// let num1 = +prompt("Enter 1st number");
+// let num2 = +prompt("Enter 2nd number");
+// let num3 = +prompt("Enter 3rd number");
+// let num4 = +prompt("Enter 4th number");
+// let num5 = +prompt("Enter 5th number");
+// if(num1>(num2,num3,num4,num5)){
+// console.log("Larger number is " + num1);
+// }
+// else if(num2>(num1,num3,num4,num5)){
+//     console.log("Larger number is " + num2);
+//     }
+// else if(num3>(num2,num1,num4,num5)){
+//     console.log("Larger number is " + num3);
+//     }
+// else if(num4>(num2,num1,num3,num5)){
+//     console.log("Larger number is " + num4);
+//     }
+// else{
+//     console.log("Larger number is " + num5);
+//     }   
+
+//Q4) Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message for the screen.
+// for( let i=0; i<15; i++){
+//     let number= +prompt("Enter a number")
+//     if(number%2===0){
+// console.log(number + " is even");
+//     }
+//     else{
+//         console.log(number + " is odd")
+//     }
+// }
+
+//Q5) Write a Javascript program which computes the average marks of the following students then,this average is used to determine the corresponding grade.
+// let English = +prompt("Enter English marks");
+// let Urdu = +prompt("Enter Urdu marks");
+// let Maths = +prompt("Enter Maths marks");
+// let Science = +prompt("Enter Science marks");
+// let computer = +prompt("Enter computer marks");
+// let Average = (English + Urdu + Maths + Science + computer)/5;
+// if(Average<=100 && Average>=90){
+// console.log("Grade : A Congratulations! You are pass");
+// }
+// else if(Average<90 && Average>=80){
+// console.log("Grade : B Congratulations! You are pass");
+// }
+// else if(Average<80 && Average>=70){
+// console.log("Grade : C Congratulations! You are pass");
+// }
+// else if(Average<70 && Average>=60){
+// console.log("Grade : D Congratulations! You are pass");
+// }
+// else if(Average<60){
+// console.log("Grade : F You are fail");
+// }
+// else{
+//     console.log("You enter invalid number")
+// }
+
+//Q6) Write Javascript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of number and for the multiples of five print "Buzz".For numbers which are multiples of both three and five prints "FizzBuzz".
+// for(let i=1; i<=100; i++){
+//     if(i%3===0){
+//         console.log("Fizz");
+//     }
+// else if(i%5===0){
+//     console.log("Buzz");
+// }
+// else if(i%15===0){
+//     console.log("FizzBuzz");
+// }
+// else{
+//     console.log(i);
+// }
+//  }
+
+//Q7) Write a JavaScript program to construct the following pattern, using a nested for loop.
+// let n = 5;
+// let space = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     space += "*";
+//   }
+//   space += "\n";
+// }
+// console.log(space);
